72f7be3bb72f3edc4c522d8a985b86be66240e82
tejaswi531 <54668200+tejaswi531@users.noreply.github.com>
2022-10-03 10:38:47 +0530

    Add files via upload

---
+++ zuul-api-gateway-server/zuul-api-gateway-server/src/main/java/com/javatpoint/microservices/zuulapigatewayserver/ZuulApiGatewayServerApplication.java
@@ -0,0 +1,14 @@
+package com.javatpoint.microservices.zuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class ZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayServerApplication.class, args);
+	}
+
+}
